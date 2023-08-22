58a36883b4c08f3cc737ff1ca0bd4e50fae53934
ciwwan1980 <ajilqado@gmail.com>
2023-08-22 15:31:10 +0200

    task3

---
+++ task3.py
@@ -0,0 +1,36 @@
+# Write a Python script that takes multiple characters and assigns these characters to a variable using the "+=". Print the value of the variable.
+
+# - Your result should look like this:
+
+# ```
+# The resulting string is: dlroW olleH
+# ```
+
+char1 = "H"
+char2 = "e"
+char3 = "l"
+char4 = "l"
+char5 = "o"
+char6 = " "
+char7 = "W"
+char8 = "o"
+char9 = "r"
+char10 = "l"
+char11 = "d"
+
+
+
+reverse = "d"
+reverse += "l"
+reverse += "r"
+reverse += "o"
+reverse += "w"
+reverse += " "
+reverse += "o"
+reverse += "l"
+reverse += "l"
+reverse += "e"
+reverse += "h"
+
+
+print("The resulting string is:", reverse)
