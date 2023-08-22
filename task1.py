c51a0bd0134d7df3a4e902809975a1e99de2124c
ciwwan1980 <ajilqado@gmail.com>
2023-08-22 15:06:38 +0200

    te first task

---
+++ task1.py
@@ -0,0 +1,19 @@
+
+# Write a Python script that takes two integers and assigns their sum to a variable using the "+=" operator. Print the value of the variable.
+
+# - Your result should look like this:
+
+# ```
+# The sum is 15
+
+
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+
+sum_result = num1 # for example 5
+sum_result += num2 # for example 10
+# the result will be 15
+
+print("The sum is", sum_result)
